57ca3fc5f1e7007917706216b709a881e9ec1428
hakutyo0828 <masamune615msmn@gmail.com>
2025-04-20 13:00:18 +0900

    AWSリソースの作成

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+    required_version = "1.11.4"
+  backend "s3" {
+    bucket  = "hakutyo-terraform-state"
+    key     = "terraform.state"
+    region  = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
